7e48112c967f59786a2c92a67e2eb853320d7832
roarou <gcherif@student.ethz.ch>
2023-07-28 11:04:39 +0200

    Add monitring

--- Model/get_hyperparameters/learning_rate_finder.py
+++ Model/get_hyperparameters/learning_rate_finder.py
@@ -1,12 +1,9 @@
 import pytorch_lightning as pl
 from torch.utils.data import Dataset, DataLoader
-from torch_lr_finder import LRFinder
-import torch
-import torch.optim as optim
-from torch import nn
 from Model.pointnet import SpineSegmentationNet
 from Model.pointcloud_normalization import normalize_point_cloud
-from get_metrics import calculate_metrics
+from pytorch_lightning.loggers import TensorBoardLogger
+from pytorch_lightning.callbacks import ModelCheckpoint, LearningRateMonitor
 
 
 # Define Dataset
@@ -21,6 +18,19 @@ class PointCloudDataset(Dataset):
         point_cloud, labels = normalize_point_cloud(self.file_list[idx])  # Adjust according to your function
         return point_cloud, labels
 
+logger = TensorBoardLogger("tb_logs", name="Spine_Segmentation")
+
+# Define Learning Rate monitor
+lr_monitor = LearningRateMonitor(logging_interval='step')
+
+# Model Checkpoint callback
+checkpoint_callback = ModelCheckpoint(
+    monitor='val_loss',
+    dirpath='my_model/',
+    filename='my_model-{epoch:02d}-{val_loss:.2f}',
+    save_top_k=3,
+    mode='min',
+)
 
 # Load your dataset
 # Suppose you have a list of point cloud files for training
@@ -29,10 +39,16 @@ dataset = PointCloudDataset(train_files)
 dataloader = DataLoader(dataset, batch_size=32, shuffle=True)
 
 # Define your model
-model = SpineSegmentationModule()
+model = SpineSegmentationNet()
 
 # Use PyTorch Lightning's built-in learning rate finder
-trainer = pl.Trainer(gpus=1, max_epochs=1)
+# PyTorch Lightning trainer with Tensorboard Logger and Callbacks
+trainer = pl.Trainer(
+    gpus=1,
+    max_epochs=10,
+    logger=logger,
+    callbacks=[lr_monitor, checkpoint_callback],
+)
 lr_finder = trainer.tuner.lr_find(model, dataloader)
 
 # Plot learning rate finder results
@@ -48,3 +64,6 @@ model.learning_rate = new_lr
 # Train the model
 trainer = pl.Trainer(gpus=1, max_epochs=100)
 trainer.fit(model, dataloader)
+
+# Test the model
+trainer.test(model, dataloader)
