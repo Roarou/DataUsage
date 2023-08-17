fixed normalization by getting rid of nan values

Model/load_dataset.py
@@ -5,6 +5,7 @@ from torch.utils.data import Dataset
 from sklearn.model_selection import train_test_split
 import torch
 
+
 class PointcloudDataset(Dataset):
     def __init__(self, base_path=r'G:\SpineDepth', split='train', test_size=0.1, val_size=0.1, random_state=42):
         """
@@ -19,7 +20,6 @@ class PointcloudDataset(Dataset):
         """
         self.base_path = base_path
         self.specimen_dirs = sorted([d for d in os.listdir(base_path) if d.startswith('Specimen')])
-
         # Split the dataset into train, validation, and test sets
         train_dirs, test_dirs = train_test_split(self.specimen_dirs, test_size=test_size, random_state=random_state)
         train_dirs, val_dirs = train_test_split(train_dirs, test_size=val_size, random_state=random_state)
@@ -53,48 +53,53 @@ class PointcloudDataset(Dataset):
 
         input_data = []
         labels = []
-
+        print('test2')
         for recording_num in range(1, 41):
-            recording_dir = os.path.join(recordings_dir, f'Recording_{recording_num}')
+            recording_dir = os.path.join(recordings_dir, f'Recording{recording_num}')
+            recording_dir = os.path.join(recording_dir, 'pointcloud')
+
             for video_num in range(2):  # Iterate over 'Video_0' and 'Video_1'
                 video_dir = os.path.join(recording_dir, f'Video_{video_num}')
                 groundtruth_dir = os.path.join(video_dir, 'Groundtruth')
                 input_files = sorted([f for f in os.listdir(video_dir) if f.endswith('.pcd')])
 
                 for input_file in input_files:
-                    input_pcd = o3d.io.read_point_cloud(os.path.join(video_dir, input_file))
+                    input_pcd = o3d.io.read_point_cloud(os.path.join(video_dir, input_file), remove_nan_points=True, remove_infinite_points=True)
                     groundtruth_pcd = o3d.io.read_point_cloud(
                         os.path.join(groundtruth_dir, input_file.replace('.pcd', '_GT.pcd')))
 
                     # Normalize input data
-                    normalized_input = self.normalize_point_cloud(np.array(input_pcd.points))
-
+                    input = np.asarray(input_pcd.points)
+                    normalized_input = self.normalize_point_cloud(input)
                     # Extract labels (binary values based on color)
-                    labeled_pcd = np.array(groundtruth_pcd.colors)[:, 0]
+                    labeled_pcd = np.asarray(groundtruth_pcd.colors)[:, 0]
                     binary_labels = (labeled_pcd >= 0.5).astype(np.float32)
-
                     input_data.append(normalized_input)
                     labels.append(binary_labels)
 
         input_data = np.array(input_data)
         labels = np.array(labels)
+        print(labels)
         input_data = torch.tensor(input_data, dtype=torch.float32)
         labels = torch.tensor(labels, dtype=torch.float32)
 
         return input_data, labels
 
     def normalize_point_cloud(self, points):
-        """
-        Normalizes point cloud data.
+        if np.any(np.isnan(points)):
+            raise ValueError("Input data contains NaN values.")
 
-        Args:
-            points (np.ndarray): Point cloud data.
-
-        Returns:
-            np.ndarray: Normalized point cloud data.
-        """
         centroid = np.mean(points, axis=0)
+
+        if np.any(np.isnan(centroid)):
+            raise ValueError("Centroid calculation resulted in NaN values.")
+
         points -= centroid  # center
-        furthest_distance = np.max(np.sqrt(np.sum(abs(points) ** 2, axis=-1)))
+        furthest_distance = np.max(np.sqrt(np.sum(points ** 2, axis=-1)))
+
+        if furthest_distance == 0.0:
+            raise ValueError("Furthest distance is zero, which can cause division by zero.")
+
         points /= furthest_distance  # scale
         return points
+