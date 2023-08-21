63b6ece840b2930169745b202283e65fc0aa74dc
roarou <gcherif@student.ethz.ch>
2023-08-21 17:37:07 +0200

    New approach

--- Model/pointnet.py
+++ Model/pointnet.py
@@ -1,104 +1,51 @@
-import torch
 import torch.nn as nn
-from torch_geometric.nn import knn
-
-
-class SetAbstraction(nn.Module):
-    def __init__(self, in_channels, out_channels, sample_points, group_points):
-        super().__init__()
-        self.sample_points = sample_points
-        self.group_points = group_points
-        self.mlp = nn.Sequential(
-            nn.Conv1d(in_channels, out_channels, 1),
-            nn.ReLU(),
-            nn.Conv1d(out_channels, out_channels, 1)
-        )
-
-    def forward(self, x):
-        # Get batch size, number of points, and number of features
-        batch_size, num_points, num_features = x.size()
-
-        # List to collect the output for each point cloud in the batch
-        features_list = []
-        sampled_x_list = []
-
-        # Process each point cloud in the batch
-        for i in range(batch_size):
-            # Inside the loop, make sure both point_cloud and sampled_x are 2D tensors
-            print(i)
-            point_cloud = x[i, :, :]
-
-            idx = torch.randperm(num_points)[:self.sample_points]
-            sampled_x = point_cloud[idx, :]
-
-            # Ensure that both tensors are 2D before passing them to the KNN function
-            neighbors = knn(point_cloud, sampled_x, self.group_points)
-            neighbors = neighbors[1]
-
-            # Group neighbors
-            groups = point_cloud[neighbors, :]
-            groups = groups.permute(1, 0)
-
-            # Apply MLPs
-            features = self.mlp(groups)
-
-            # Max Pooling
-            features, _ = torch.max(features, dim=1)
-
-            # Append to the list
-            features_list.append(features)
-            sampled_x_list.append(sampled_x)
-
-
-        # Stack results to get the batched output
-
-        # Add the missing dimension back (assuming it should be added as the third dimension)
-        features_batch = torch.cat(features_list, dim=0).view(batch_size, -1, self.sample_points)
-        sampled_x_batch = torch.cat(sampled_x_list, dim=0).view(batch_size, -1, self.sample_points)
-
-        print(features_batch.shape)
-        print(sampled_x_batch.shape)
-        return features_batch, sampled_x_batch
-
-class FeaturePropagation(nn.Module):
-    def __init__(self, in_channels, out_channels):
-        super().__init__()
-        self.mlp = nn.Sequential(
-            nn.Conv1d(in_channels, out_channels, 1),
-            nn.ReLU(),
-            nn.Conv1d(out_channels, out_channels, 1)
-        )
-
-    def forward(self, x, sampled_x, features):
-        # Interpolate features
-        dists, idx = knn(sampled_x, x, 3)
-        interpolated_features = torch.sum(features[:, idx, :], dim=2) / 3
-
-        # Concatenate with input features
-        x = torch.cat([x, interpolated_features], dim=-1)
-
-        # Apply MLP
-        x = self.mlp(x)
-
-        return x
-
+import torch.nn.functional as F
+from models.pointnet2_utils import PointNetSetAbstractionMsg, PointNetFeaturePropagation
 
 class SpineSegmentationNet(nn.Module):
     def __init__(self):
-        super().__init__()
-        self.sa1 = SetAbstraction(3, 64, 512, 32)
-        self.sa2 = SetAbstraction(64, 128, 128, 32)
-        self.fp1 = FeaturePropagation(128 + 64, 64)
-        self.fp2 = FeaturePropagation(64 + 3, 32)
-        self.fc = nn.Linear(32, 2)  # Binary classification
+        super(SpineSegmentationNet, self).__init__()
 
-    def forward(self, x):
-        x1, s1 = self.sa1(x)
-        x2, s2 = self.sa2(x1)
+        # Set abstraction layers for hierarchical point cloud processing
+        self.set_abstraction_layer1 = PointNetSetAbstractionMsg(1024, [0.05, 0.1], [16, 32], 9, [[16, 16, 32], [32, 32, 64]])
+        self.set_abstraction_layer2 = PointNetSetAbstractionMsg(256, [0.1, 0.2], [16, 32], 32+64, [[64, 64, 128], [64, 96, 128]])
+        self.set_abstraction_layer3 = PointNetSetAbstractionMsg(64, [0.2, 0.4], [16, 32], 128+128, [[128, 196, 256], [128, 196, 256]])
+        self.set_abstraction_layer4 = PointNetSetAbstractionMsg(16, [0.4, 0.8], [16, 32], 256+256, [[256, 256, 512], [256, 384, 512]])
 
-        x2 = self.fp1(x1, s1, x2)
-        x2 = self.fp2(x, s2, x2)
+        # Feature propagation layers
+        self.feature_propagation_layer4 = PointNetFeaturePropagation(512+512+256+256, [256, 256])
+        self.feature_propagation_layer3 = PointNetFeaturePropagation(128+128+256, [256, 256])
+        self.feature_propagation_layer2 = PointNetFeaturePropagation(32+64+256, [256, 128])
+        self.feature_propagation_layer1 = PointNetFeaturePropagation(128, [128, 128, 128])
 
-        x = self.fc(x2)
+        # Convolution layers
+        self.conv_layer1 = nn.Conv1d(128, 128, 1)
+        self.batch_norm1 = nn.BatchNorm1d(128)
+        self.dropout_layer = nn.Dropout(0.5)
+        self.conv_layer2 = nn.Conv1d(128, 1, 1) # Segmentation layer for binary classification per point
+        self.sigmoid_activation = nn.Sigmoid()
 
-        return torch.sigmoid(x)
+    def forward(self, point_cloud_xyz):
+        # Input layer
+        input_points = point_cloud_xyz
+        input_xyz_coordinates = point_cloud_xyz[:,:3,:]
+
+        # Set abstraction layers
+        l1_xyz, l1_points = self.set_abstraction_layer1(input_xyz_coordinates, input_points)
+        l2_xyz, l2_points = self.set_abstraction_layer2(l1_xyz, l1_points)
+        l3_xyz, l3_points = self.set_abstraction_layer3(l2_xyz, l2_points)
+        l4_xyz, l4_points = self.set_abstraction_layer4(l3_xyz, l3_points)
+
+        # Feature propagation layers
+        l3_points = self.feature_propagation_layer4(l3_xyz, l4_xyz, l3_points, l4_points)
+        l2_points = self.feature_propagation_layer3(l2_xyz, l3_xyz, l2_points, l3_points)
+        l1_points = self.feature_propagation_layer2(l1_xyz, l2_xyz, l1_points, l2_points)
+        input_points = self.feature_propagation_layer1(input_xyz_coordinates, l1_xyz, None, l1_points)
+
+        # Convolution layers
+        x = self.dropout_layer(F.relu(self.batch_norm1(self.conv_layer1(input_points))))
+        x = self.conv_layer2(x)
+        segmentation_mask = self.sigmoid_activation(x)
+        segmentation_mask = segmentation_mask.permute(0, 2, 1)
+
+        return segmentation_mask, l4_points
