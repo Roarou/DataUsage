bab0a48add048d21859dfd7c019f563438daaacf
roarou <gcherif@student.ethz.ch>
2023-10-04 08:54:49 +0200

    generalized variable

--- DataProcessing/FileManaging/uniform move files.py
+++ DataProcessing/FileManaging/uniform move files.py
@@ -3,23 +3,24 @@ import shutil
 import numpy as np
 from tqdm import tqdm
 
-source_folder = 'L:\\groundtruth_labeled'
+source_folder = 'L:\\groundtruth'
 destination_folder = 'L:\\backup_files'
 
+nb_files_to_move = 80000
 # List all files in the source folder
 all_files = os.listdir(source_folder)
 total_files = len(all_files)
-pbar = tqdm(total=100000)
+pbar = tqdm(total=nb_files_to_move)
 
 # If there are fewer than 100,000 files, move them all
-if total_files <= 100000:
+if total_files <= nb_files_to_move:
     for filename in all_files:
         shutil.move(os.path.join(source_folder, filename), os.path.join(destination_folder, filename))
         pbar.update(1)
 
 else:
     # Otherwise, uniformly select and move 100,000 files
-    selected_indices = np.linspace(0, total_files - 1, 100000, dtype=int)
+    selected_indices = np.linspace(0, total_files - 1, nb_files_to_move, dtype=int)
 
     for i in selected_indices:
         filename = all_files[i]
