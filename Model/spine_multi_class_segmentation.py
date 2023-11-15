124491ada4595a88d511cd23c9e100de20ac93b4
roarou <gcherif@student.ethz.ch>
2023-11-15 02:35:59 +0100

    Training running and IOU promising

--- Model/spine_multi_class_segmentation.py
+++ Model/spine_multi_class_segmentation.py
@@ -1,58 +1,100 @@
 import torch.nn as nn
 import torch.nn.functional as F
-from Model.utils import PointNetSetAbstractionMsg, PointNetFeaturePropagation, PointNetSetAbstraction
-import torch
+from utils import PointNetSetAbstractionMsg,PointNetFeaturePropagation, PointNetSetAbstraction
 
+class FastNet(nn.Module):
+    def __init__(self, num_classes):
+        super(FastNet, self).__init__()
 
-class SpineSegmentationNet(nn.Module):
-
-    def __init__(self, num_classes=6, normal_channel=False):
-        super(SpineSegmentationNet, self).__init__()
-        extra_channels = 3 if normal_channel else 0
-        self.include_normal_channels = normal_channel
-        self.abstraction1 = PointNetSetAbstractionMsg(512, [0.1, 0.2, 0.4], [32, 64, 128], 3 + extra_channels,
+        self.sa1 = PointNetSetAbstractionMsg(512, [0.1, 0.2, 0.4], [32, 64, 128], 3,
                                                       [[32, 32, 64], [64, 64, 128],
                                                        [64, 96, 128]])
-        self.abstraction2 = PointNetSetAbstraction(npoint=None, radius=None, nsample=None, in_channel=320 + 3,
+        self.sa2 = PointNetSetAbstractionMsg(128, [0.4, 0.8], [64, 128], 128 + 128 + 64,
+                                                      [[128, 128, 256],
+                                                       [128, 196, 256]])
+        self.sa3 = PointNetSetAbstraction(npoint=None, radius=None, nsample=None, in_channel=512 + 3,
                                                    mlp=[256, 512, 1024],
                                                    group_all=True)
-        self.feature_propagation2 = PointNetFeaturePropagation(in_channel=1344,
+        self.feature_propagation3 = PointNetFeaturePropagation(in_channel=1536,
+                                                               mlp=[256, 256])
+        self.feature_propagation2 = PointNetFeaturePropagation(in_channel=576,
                                                                mlp=[256, 128])
-        self.feature_propagation1 = PointNetFeaturePropagation(in_channel=131 + extra_channels,
+        self.feature_propagation1 = PointNetFeaturePropagation(in_channel=131,
                                                                mlp=[128, 128])
-        self.conv_layer1 = nn.Conv1d(128, 128, 1)
-        self.batch_norm1 = nn.BatchNorm1d(128)
-        self.conv_layer2 = nn.Conv1d(128, num_classes, 1)
+        self.conv1 = nn.Conv1d(128, 128, 1)
+        self.bn1 = nn.BatchNorm1d(128)
+        self.drop1 = nn.Dropout(0.5)
+        self.conv2 = nn.Conv1d(128, num_classes, 1)
 
-        # Rest of the class methods (e.g., forward) remains the same
+    def forward(self, xyz):
+        l0_points = xyz
+        l0_xyz = xyz
 
-    def forward(self, point_cloud_xyz):
 
-        # Set Abstraction layers
-        # print(f'Input xyz shape: {point_cloud_xyz.shape}')
-        if self.include_normal_channels:  # If the normal_channel flag is True
-            # Input layer
-            input_points = point_cloud_xyz.permute(0, 2, 1)
-            input_xyz_coordinates = point_cloud_xyz[:, :, :3].permute(0, 2, 1)
-        else:
-            # Input layer
-            input_points = point_cloud_xyz.permute(0, 2, 1)
-            input_xyz_coordinates = point_cloud_xyz.permute(0, 2, 1)
-        l1_xyz, l1_points = self.abstraction1(input_xyz_coordinates,
-                                              input_points)  # Applying first set abstraction layer
-        l2_xyz, l2_points = self.abstraction2(l1_xyz, l1_points)  # Applying second set abstraction layer
+        l1_xyz, l1_points = self.sa1(l0_xyz, l0_points)
+        l2_xyz, l2_points = self.sa2(l1_xyz, l1_points)
+        l3_xyz, l3_points = self.sa3(l2_xyz, l2_points)
+
         # Feature Propagation layers
+        l2_points = self.feature_propagation3(l2_xyz, l3_xyz, l2_points,
+                                              l3_points)  # Applying first feature propagation layer
         l1_points = self.feature_propagation2(l1_xyz, l2_xyz, l1_points,
                                               l2_points)  # Applying second feature propagation layer
-        l0_points = self.feature_propagation1(input_xyz_coordinates, l1_xyz, input_points, l1_points)
+        l0_points = self.feature_propagation1(xyz, l1_xyz, l0_points, l1_points)
+
+        x = self.drop1(F.relu(self.bn1(self.conv1(l0_points))))
+        x = self.conv2(x)
+        x = F.log_softmax(x, dim=1)
+        x = x.permute(0, 2, 1)
+        return x, l3_points
+class SpineSegmentationNet(nn.Module):
+    def __init__(self, num_classes):
+        super(SpineSegmentationNet, self).__init__()
+
+        self.sa1 = PointNetSetAbstractionMsg(1024, [0.05, 0.1], [16, 32], 3, [[16, 16, 32], [32, 32, 64]])
+        self.sa2 = PointNetSetAbstractionMsg(256, [0.1, 0.2], [16, 32], 32+64, [[64, 64, 128], [64, 96, 128]])
+        self.sa3 = PointNetSetAbstractionMsg(64, [0.2, 0.4], [16, 32], 128+128, [[128, 196, 256], [128, 196, 256]])
+        self.sa4 = PointNetSetAbstractionMsg(16, [0.4, 0.8], [16, 32], 256+256, [[256, 256, 512], [256, 384, 512]])
+        self.fp4 = PointNetFeaturePropagation(512+512+256+256, [256, 256])
+        self.fp3 = PointNetFeaturePropagation(128+128+256, [256, 256])
+        self.fp2 = PointNetFeaturePropagation(32+64+256, [256, 128])
+        self.fp1 = PointNetFeaturePropagation(128, [128, 128, 128])
+        self.conv1 = nn.Conv1d(128, 128, 1)
+        self.bn1 = nn.BatchNorm1d(128)
+        self.drop1 = nn.Dropout(0.5)
+        self.conv2 = nn.Conv1d(128, num_classes, 1)
+
+    def forward(self, xyz):
+        l0_points = xyz
+        l0_xyz = xyz
+
+        l1_xyz, l1_points = self.sa1(l0_xyz, l0_points)
+        l2_xyz, l2_points = self.sa2(l1_xyz, l1_points)
+        l3_xyz, l3_points = self.sa3(l2_xyz, l2_points)
+        l4_xyz, l4_points = self.sa4(l3_xyz, l3_points)
+
+        l3_points = self.fp4(l3_xyz, l4_xyz, l3_points, l4_points)
+        l2_points = self.fp3(l2_xyz, l3_xyz, l2_points, l3_points)
+        l1_points = self.fp2(l1_xyz, l2_xyz, l1_points, l2_points)
+        l0_points = self.fp1(l0_xyz, l1_xyz, None, l1_points)
+
+        x = self.drop1(F.relu(self.bn1(self.conv1(l0_points))))
+        x = self.conv2(x)
+        x = F.log_softmax(x, dim=1)
+        x = x.permute(0, 2, 1)
+        return x, l4_points
 
 
-        feat1 = self.conv_layer1(l0_points)
-        feat1 = self.batch_norm1(feat1)
-        # FC layers
-        x = F.relu(feat1)
-        x = self.conv_layer2(x)  # Ensure that conv2 has 1 output channel for binary classification
+class get_loss(nn.Module):
+    def __init__(self):
+        super(get_loss, self).__init__()
+    def forward(self, pred, target, trans_feat, weight):
+        total_loss = F.nll_loss(pred, target, weight=weight)
 
-        x = F.log_softmax(x, dim=1)  # Applying sigmoid activation for binary classification
+        return total_loss
 
-        return x, l2_points
+if __name__ == '__main__':
+    import  torch
+    model =  SpineSegmentationNet(6)
+    xyz = torch.rand(6, 9, 2048)
+    (model(xyz))
